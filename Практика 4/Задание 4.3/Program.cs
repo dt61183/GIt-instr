dbe6df8778b966e1a49473b98ee3c0282c6ed2fb
dt61183 <dt61183@gmail.com>
2022-11-17 16:17:05 +0700

    Задание 4.3

--- Практика 4/Задание 4.3/Program.cs
+++ Практика 4/Задание 4.3/Program.cs
@@ -2,14 +2,12 @@
 // далее выводит массив на экран в одну строку.
 
 int lenArray = ReadInt("Введите длинну массива: ");
-
 int[] randomArray = new int[lenArray];
 for (int i = 0; i < randomArray.Length; i++)
 {
     randomArray[i] = new Random().Next(1,9);
     Console.Write(randomArray[i] + " ");
 }
-
 int ReadInt(string message)
 {
     Console.Write(message);
