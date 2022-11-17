7b3a6ed63c27452756d097c466ae61f63b3c39a1
dt61183 <dt61183@gmail.com>
2022-11-17 18:59:34 +0700

    Задание 5.1

--- Практика 5/Задание 5.1/Program.cs
+++ Практика 5/Задание 5.1/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
 // которая покажет количество чётных чисел в массиве.
-
-// 345, 897, 568, 234 -> 2
+// [345, 897, 568, 234] -> 2
 
 int size = ReadInt("Введите размерность массива: ");
 int [] numbers = new int[size];
 
 FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
 PrintArray(numbers);
 int result = 0;
 
@@ -19,7 +19,7 @@ for (int i = 0; i < numbers.Length; i++)
 }
 if (result % 10 == 1)
 {
-    Console.WriteLine($"В массиве {result} четное число");
+    Console.WriteLine($"В массиве {numbers.Length} чисел,{result} четное число");
 }
 if (result % 10 == 2 || result % 10 == 3 || result % 10 == 4)
 {
@@ -30,25 +30,25 @@ else
     Console.WriteLine($"В массиве {result} четных чисел");
 
 
-void FillArrayRandomNumbers(int [] array) 
+void FillArrayRandomNumbers(int [] numbers) 
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < numbers.Length; i++)
     {
-        array[i] = new Random().Next(99, 999);
+       numbers[i] = new Random().Next(99, 999);
     }
 }
 
-void PrintArray(int[] array) 
+void PrintArray(int[] numbers) 
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < numbers.Length; i++)
     {
-        Console.Write(array[i] + " ");
+        Console.Write(numbers[i] + " ");
     }
     Console.WriteLine();
 }
 
 
-int ReadInt(string message)  
+int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
