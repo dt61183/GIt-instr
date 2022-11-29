9e6f162585b52954a910757956a58aa909066b35
dt61183 <dt61183@gmail.com>
2022-11-29 12:04:10 +0700

    финиш ф.1

---
+++ Финиш/Задание Ф.1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int NumbersToN(int number1, int number2)
+{
+    
+    if(number1>number2) return number2;
+    
+    else
+    {
+        Console.Write(number1+ ", ");
+        number1++;
+    
+    }
+    
+    return NumbersToN(number1++, number2);
+
+}
+
+Console.WriteLine("Enter N: ");
+int numStart = 1;
+int numFinish = Convert.ToInt32(Console.ReadLine());
+
+NumbersToN(numStart,numFinish);
